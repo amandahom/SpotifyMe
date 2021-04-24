3316c4c04006d4dd9341931dabe12d0b5bf8d83c
Mandy Hom <amandakhom@gmail.com>
2021-04-24 16:00:58 -0700

    Fixed unknown objects

--- assets/components/FollowedArtists.tsx
+++ assets/components/FollowedArtists.tsx
@@ -187,18 +187,20 @@ function FollowedArtists() {
             </p>
           </div>
         </div>
-        <div className="px-6 pt-4 pb-2">
-          {followers.genres.map((genres: String, index: number) => {
-            return (
-              <span
-                key={index}
-                className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-indigo-200"
-              >
-                <p>{genres}</p>
-              </span>
-            )
-          })}
-        </div>
+        {followers && (
+          <div className="px-6 pt-4 pb-2">
+            {followers.genres.map((genres: String, index: number) => {
+              return (
+                <span
+                  key={index}
+                  className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-indigo-200"
+                >
+                  <p>{genres}</p>
+                </span>
+              )
+            })}
+          </div>
+        )}
       </div>
     )
   }
@@ -212,11 +214,13 @@ function FollowedArtists() {
   } else {
     return (
       <div>
-        <div className="p-5 sm:p-10 2xl:p-10 mx-2 md:mx-4 lg:mx-10 pb-10 grid col-start-auto sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 md:gap-10 justify-items-center">
-          {followers.map((followers: followersInterface, index: number) => {
-            return <FollowersCards {...followers} key={index} />
-          })}
-        </div>
+        {followers && (
+          <div className="p-5 sm:p-10 2xl:p-10 mx-2 md:mx-4 lg:mx-10 pb-10 grid col-start-auto sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 md:gap-10 justify-items-center">
+            {followers.map((followers: followersInterface, index: number) => {
+              return <FollowersCards {...followers} key={index} />
+            })}
+          </div>
+        )}
         {showNextButton ? (
           <div>
             <div className="flex justify-center mx-10">
--- assets/components/TopArtists.tsx
+++ assets/components/TopArtists.tsx
@@ -166,18 +166,20 @@ function TopArtists() {
             </p>
           </div>
         </div>
-        <div className="px-6 pt-4 pb-2">
-          {topArtists.genres.map((genres: String, index: number) => {
-            return (
-              <span
-                key={index}
-                className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-indigo-200"
-              >
-                <p>{genres}</p>
-              </span>
-            )
-          })}
-        </div>
+        {topArtists && (
+          <div className="px-6 pt-4 pb-2">
+            {topArtists.genres.map((genres: String, index: number) => {
+              return (
+                <span
+                  key={index}
+                  className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-indigo-200"
+                >
+                  <p>{genres}</p>
+                </span>
+              )
+            })}
+          </div>
+        )}
       </div>
     )
   }
@@ -191,11 +193,13 @@ function TopArtists() {
   } else {
     return (
       <div>
-        <div className="p-5 sm:p-10 2xl:p-10 mx-2 md:mx-4 lg:mx-10 pb-10 grid col-start-auto sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 md:gap-10 justify-items-center">
-          {topArtists.map((topArtists: topArtistsInterface, index: number) => {
-            return <TopArtistsCards {...topArtists} key={index} />
-          })}
-        </div>
+        {topArtists && (
+          <div className="p-5 sm:p-10 2xl:p-10 mx-2 md:mx-4 lg:mx-10 pb-10 grid col-start-auto sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 md:gap-10 justify-items-center">
+            {topArtists.map((topArtists: topArtistsInterface, index: number) => {
+              return <TopArtistsCards {...topArtists} key={index} />
+            })}
+          </div>
+        )}
         {showNextButton ? (
           <div>
             <div className="flex justify-center mx-10">
