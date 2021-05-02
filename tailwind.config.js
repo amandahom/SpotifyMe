f99a2e06d5e205a1481c45352cf10b9587158c20
Mandy Hom <amandakhom@gmail.com>
2021-05-01 19:43:38 -0700

    Fixing tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './components/*.{js,ts,jsx,tsx}',
-    './pages/*.{js,ts,jsx,tsx}',
+    './assets/components/*.{js,ts,jsx,tsx}',
+    './assets/components/**/*.{js,ts,jsx,tsx}',
+    './assets/**/*.{js,ts,jsx,tsx}',
+    './assets/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
